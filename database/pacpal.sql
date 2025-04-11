39a355fa34f98d61ac257664320837a581f0d1cd
Rangat001 <rgtwork114@gmail.com>
2025-04-11 23:40:35 +0530

    db

--- database/pacpal.sql
+++ database/pacpal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2025 at 07:04 PM
+-- Generation Time: Apr 11, 2025 at 08:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -11,10 +11,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-DROP DATABASE IF EXISTS `pacpal`;
-CREATE DATABASE `pacpal`;
-USE `pacpal`;
-
+DROP DATABASE IF EXISTS `pascal`;
+CREATE DATABASE `pascal`;
+USE `pascal`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
